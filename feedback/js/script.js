0bda8b385320833459213f780d3a17236f2d60d8
Denis <baidarovden@gmail.com>
2014-12-11 15:02:46 +0300

    delete photos

--- feedback/js/script.js
+++ feedback/js/script.js
@@ -1,5 +1,5 @@
 (function () {
-
+    //Блок обработки счетчиков
     var check_val = function (val, type) {
         if (isNaN(parseInt(val, 10))) {
             return 0;
@@ -41,6 +41,55 @@
         counter.addEventListener('click', onClick);
     }
 
+
+    //Работа с загруженными картинками.
+
+    var delete_foto = function clickHandler(event) {
+
+        event.preventDefault();
+
+        this.parentNode.style.display = "none";
+    };
+
+    var photos = document.querySelectorAll(".delete-photo");
+
+    for (var i = 0; i < photos.length; i++) {
+        var photo = photos[i];
+        photo.addEventListener('click', delete_foto);
+    }
+
+    var form = document.querySelector(".response");
+    var files = "";
+
+    form.querySelector(".pictures").addEventListener("change", function () {
+        files = this.files;
+
+        previewImages(files);
+    });
+
+    function previewImages(files) {
+
+        for (var i = 0; i < files.length; i++) {
+
+            var file = files[i];
+
+            if (file.type.match(/image.*/)) {
+
+                var reader = new FileReader();
+
+                reader.onload = function (event) {
+
+                    console.log(event.target.result);
+
+                };
+
+                reader.readAsDataURL(file);
+
+            }
+
+        }
+
+    }
 
 
 })();
